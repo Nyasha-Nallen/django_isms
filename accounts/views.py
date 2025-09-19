c905f086e12eeff66900f0c80347609098514dbf
Nyasha Nallen Tumba <nyashatumba10@gmail.com>
2025-09-19 12:10:23 +0200

    User Registration and user authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import CustomUserCreationForm, CustomAuthForm
+from .models import CustomUser
 
 # Create your views here.
+def register_staff(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.user_role = 'STAFF'
+            user.save()
+            messages.success(request, 'Your account created successfully. You can now log in.')
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/register.html', context)
+
+def login_view(request):
+    if request.method == 'POST':
+        form = CustomAuthForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Welcome back, {user.username}!")
+                return redirect("dashboard")
+            else:
+                messages.error(request, "Invalid username or password")
+    else:
+        form = CustomAuthForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+@login_required
+def logout_view(request):
+    logout(request)
+    messages.info(request, "You have been logged out.")
+    return redirect("login")
+
+@login_required
+def dashboard_view(request):
+    user = request.user
+    context={
+        'user':user,
+    }
+    return render(request, 'accounts/dashboard.html', context)
